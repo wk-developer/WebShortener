021a70f6ea126b7d5c63e903bf86b97bc10a997f
WiseKing Joshua <60327437+wk-developer@users.noreply.github.com>
2025-03-22 17:53:58 +0100

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,8 +1,6 @@
-import { load } from "https://deno.land/std@0.154.0/dotenv/mod.ts";
+import "https://deno.land/std@0.154.0/dotenv/load.ts";
 import { cleanEnv, url } from "https://deno.land/x/envalid@0.1.2/mod.ts";
 
-const env = await load({ export: true });
-
-export default cleanEnv(env, {
+export default cleanEnv(Deno.env.toObject(), {
   MONGO_URL: url(),
 });
