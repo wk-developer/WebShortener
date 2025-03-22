24a93f1b2b076d5f62091d66ac2de027d771e8a9
WiseKing Joshua <60327437+wk-developer@users.noreply.github.com>
2025-03-22 17:08:01 +0100

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 /*
 (c) @xditya
 View the license: https://github.com/xditya/WebShortener/blob/master/LICENSE
-*/
+
 
 import { config } from "https://deno.land/std@0.154.0/dotenv/mod.ts";
 import { cleanEnv, url } from "https://deno.land/x/envalid@0.1.2/mod.ts";
@@ -10,4 +10,16 @@ await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
   MONGO_URL: url(),
+});
+*/
+import { cleanEnv, str } from "https://deno.land/x/envalid@0.1.2/envalid.ts";
+
+export const env = cleanEnv(Deno.env.toObject(), {
+  API_KEY: str(),
+}, {
+  reporter: ({ errors }) => {
+    if (Object.keys(errors).length > 0) {
+      console.error("Invalid environment variables:", errors);
+    }
+  },
 });
