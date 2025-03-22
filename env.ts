83155115bb508463996d2312436e58e5106a0e4b
WiseKing Joshua <60327437+wk-developer@users.noreply.github.com>
2025-03-22 17:43:34 +0100

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,14 +1,8 @@
-/*
-(c) @xditya
-View the license: https://github.com/xditya/WebShortener/blob/master/LICENSE
-*/
-
-import { config } from "https://deno.land/std@0.154.0/dotenv/mod.ts";
+import { load } from "https://deno.land/std@0.154.0/dotenv/mod.ts";
 import { cleanEnv, url } from "https://deno.land/x/envalid@0.1.2/mod.ts";
 
-await config({ export: true });
+const env = await load({ export: true });
 
-export default cleanEnv(Deno.env.toObject(), {
+export default cleanEnv(env, {
   MONGO_URL: url(),
 });
-
