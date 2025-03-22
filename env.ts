e88d035725ebfb7ca6000d2acb6ab6d9e3009a86
WiseKing Joshua <60327437+wk-developer@users.noreply.github.com>
2025-03-22 17:26:32 +0100

    Update env.ts

--- env.ts
+++ env.ts
@@ -9,6 +9,6 @@ import { cleanEnv, url } from "https://deno.land/x/envalid@0.1.2/mod.ts";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  MONGO_URL: url(),
+  mongodb://wkdevs:<TEEtXVCw0OPMFSwY>@clusterbrieflyn-shard-00-00.v6l6z.mongodb.net:27017,clusterbrieflyn-shard-00-01.v6l6z.mongodb.net:27017,clusterbrieflyn-shard-00-02.v6l6z.mongodb.net:27017/?replicaSet=atlas-mersvm-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=ClusterBrieflyn(),
 });
 
